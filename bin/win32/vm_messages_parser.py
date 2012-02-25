a65080cee0f1019aafca75dda32632fddeed0926
Asim Ihsan <angry.blade5619@ihsan.io>
2012-02-25 22:16:42 +0000

    Improve tests, implement mock Python parser. - Add a test that break two concatenated lines in half. This will give the full possible range of possible fragmentation. - Python parser for example virtual machine logs is done. Much more complex than I thought it'd be.

--- bin/win32/vm_messages_parser.py
+++ bin/win32/vm_messages_parser.py
@@ -12,8 +12,9 @@ import datetime
 import re
 import json
 
+APP_NAME = "vm_messages_parser"
 import logging
-logger = logging.getLogger("vm_messages_parser")
+logger = logging.getLogger(APP_NAME)
 logger.setLevel(logging.DEBUG)
 ch = logging.StreamHandler()
 ch.setLevel(logging.DEBUG)
@@ -23,8 +24,27 @@ logger.addHandler(ch)
 
 class LogDatum(object):
     def __init__(self, string_input):
-        self.string_input = string_input
+        self.string_input = string_input        
         
+    # ------------------------------------------------------------------------
+    #   Format of the datetime at the start of the line.
+    # ------------------------------------------------------------------------
+    DATETIME_FORMAT = "%Y-%b-%d %H:%M:%S"
+    # ------------------------------------------------------------------------
+    
+    # ------------------------------------------------------------------------
+    #   Regular expression to get elements out of the line.
+    # ------------------------------------------------------------------------
+    re1='((?:(?:[1]{1}\\d{1}\\d{1}\\d{1})|(?:[2]{1}\\d{3}))[-:\\/.](?:Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:tember)?|Sept|Oct(?:ober)?|Nov(?:ember)?|Dec(?:ember)?)[-:\\/.](?:(?:[0-2]?\\d{1})|(?:[3][01]{1})))(?![\\d])'	# YYYYMMMDD 1
+    re2='.*?'	# Non-greedy match on filler
+    re3='((?:(?:[0-1][0-9])|(?:[2][0-3])|(?:[0-9])):(?:[0-5][0-9])(?::[0-5][0-9])?(?:\\s?(?:am|AM|pm|PM))?)'	# HourMinuteSec 1
+    re4='.*?'	# Non-greedy match on filler
+    re5='((?:\S+))'	# Non-space
+    re6='\s+' # Spaces
+    re7='(.*)' # Rest of the line
+    RE_LINE = re.compile(re1+re2+re3+re4+re5+re6+re7, re.IGNORECASE | re.DOTALL)
+    # ------------------------------------------------------------------------
+    
     def get_dict_representation(self):
         """ Given a block of a full log event return a dict with the following
         required keys:
@@ -37,7 +57,9 @@ class LogDatum(object):
         -   contents: full block from the log.
         
         and the following optional keys:
-        -   millisecond: integer
+        -   microsecond: integer
+        
+        Add whatever other keys you like for subscribers to this parser.
         
         Example lines:
         
@@ -47,24 +69,56 @@ class LogDatum(object):
         - If the log outputs data in single lines contents will always be a single line.
         - If the log outputs data to multiple lines for a given datetime contents may
         be a line-delimited string.
+        
+        Return None if the input can't be parsed.
         """
     
-        return {"contents": self.string_input}
+        if len(self.string_input.splitlines()) != 1:
+            return None        
+        m = self.RE_LINE.search(self.string_input)
+        if not m:
+            return None    
+        yyyymmmdd1 = m.group(1)
+        time1 = m.group(2)
+        machine_name = m.group(3)
+        contents = m.group(4)
+        
+        full_datetime = " ".join([yyyymmmdd1, time1])
+        try:
+            datetime_obj = datetime.datetime.strptime(full_datetime, self.DATETIME_FORMAT)
+        except ValueError:
+            return None
+        return_value = {}
+        return_value["year"] = str(datetime_obj.year)
+        return_value["month"] = str(datetime_obj.month)
+        return_value["day"] = str(datetime_obj.day)
+        return_value["hour"] = str(datetime_obj.hour)
+        return_value["minute"] = str(datetime_obj.minute)
+        return_value["second"] = str(datetime_obj.second)
+        return_value["contents"] = self.string_input
+        return return_value
+        
+    def __repr__(self):
+        return "LogDatum: %s" % (self.get_dict_representation(), )
         
     def __str__(self):
-        return str(self.get_dict_representation)
+        return "%s" % (self.get_dict_representation(), )
 
 def validate_command(command):
     if "contents" not in command:
         return False
     return True
 
+re_line_breaks = re.compile("(\r\n|\n)", re.DOTALL)
+line_breaks = set(["\r\n", "\n"])
 def split_contents_and_return_excess(contents):
     """ Given a block of text in contents split it into lines, and
     return a two-element tuple (elem1, elem2).
     -   elem1: list of strings for lines in the contents. If there
         are no full lines return an empty list.
-    -   elem2: string of the excess, i.e. the last line.
+    -   elem2: string of the excess, i.e. the last line.i
+    
+    We want to strip out blank lines.
         
     Examples below:
 
@@ -72,7 +126,10 @@ def split_contents_and_return_excess(contents):
     return ([], "")
     
     contents = "12345"
-    return ([], "")
+    return ([], "12345")
+    
+    contents = "12345\r"
+    return ([], "12345\r")
     
     contents = "12345\r\n"
     return (["12345", ""])
@@ -83,43 +140,66 @@ def split_contents_and_return_excess(contents):
     contents = "12345\n123456\n123457"    
     return (["12345", "123456"], "1234567")
     
+    contents = "\r\n\r\n\r\n12345"
+    return ([], "12345")
+    
     """
     
-    if len(contents) == 0:        
-        return_value = ([], "")    
-    else:
-        # After splitting lines we need to restore line breaks
-        lines = contents.splitlines()        
-        for i, line in enumerate(lines[:-1]):
-            lines[i] = ''.join(lines[i], '\n')
-            
-        if len(lines) == 1:            
-            return_value = ([], lines[0])
-        else:        
-            return_value = (lines[:-1], lines[-1])    
+    logger = logging.getLogger("%s.split_contents_and_return_excess" % (APP_NAME, ))
+    elems = re_line_breaks.split(contents)
+    last_index = len(elems) - 1
+    non_line_break_elems = [(i, elem) for (i, elem) in enumerate(elems) 
+                            if elem not in line_breaks]
+    full_lines = [elem for (i, elem) in non_line_break_elems
+                  if len(elem) != 0 and i != last_index]
+    trailing_excess = elems[-1]
+
+    return_value = (full_lines, trailing_excess) 
     logger.debug("returning : %s" % (return_value, ))
     return return_value
 
+def get_log_data_and_excess_lines(full_lines):
+    """ Given a list of strings corresponding to full lines from log output
+    return a two-element tuple (elem1, elem2).
+    - elem1: a list of zero or more LogDatum objects that correspond to the
+    contents of the logs.
+    - elem2: a list of lines that constitute a partial log datum.
+    
+    We will silently drop input that doesn't meet the spec of a log block."""
+    
+    logger = logging.getLogger("%s.get_log_data_and_excess_lines" % (APP_NAME, ))
+    return_value = []
+    for line in full_lines:
+        log_datum = LogDatum(line)
+        return_value.append(log_datum)
+    return (return_value, [])
+    
 if __name__ == "__main__":
     logger.info("starting")
     
-    PROTOCOL = "tcp"
-    HOSTNAME = "127.0.0.1"
-    PORTS = [2000]
-    FILTER = ""
+    context = zmq.Context(2)
     
-    connections = ["%s://%s:%s" % (PROTOCOL, HOSTNAME, port) for port in PORTS]
-    logger.debug("Collecting updates from: %s" % (pprint.pformat(connections), ))
+    # ------------------------------------------------------------------------
+    #   Subscribing to the raw ssh_tap from a server.
+    # ------------------------------------------------------------------------
+    subscription_binding = "tcp://127.0.0.1:2000"    
+    subscription_socket = context.socket(zmq.SUB)
+    subscription_socket.connect(subscription_binding)
+    subscription_socket.setsockopt(zmq.SUBSCRIBE, "")
+    # ------------------------------------------------------------------------
     
-    context = zmq.Context(1)
-    socket = context.socket(zmq.SUB)
-    map(socket.connect, connections)
-    socket.setsockopt(zmq.SUBSCRIBE, FILTER)
+    # ------------------------------------------------------------------------
+    #   Publishing JSON for parsed log data.
+    # ------------------------------------------------------------------------
+    publish_binding = "tcp://127.0.0.1:3000"
+    publish_socket = context.socket(zmq.PUB)
+    publish_socket.connect(publish_binding)    
+    # ------------------------------------------------------------------------
     
     trailing_excess = ""    
     full_lines = []
     while 1:
-        incoming_string = socket.recv()
+        incoming_string = subscription_socket.recv()
         logger.debug("Update: '%s'" % (incoming_string, ))
         try:
             incoming_object = json.loads(incoming_string)
@@ -132,7 +212,18 @@ if __name__ == "__main__":
         trailing_excess = ''.join([trailing_excess, incoming_object["contents"]])     
         (new_full_lines, trailing_excess) = split_contents_and_return_excess(trailing_excess)        
         full_lines.extend(new_full_lines)
-        logger.debug("full_lines:\n%s" % (full_lines, ))
+        logger.debug("full_lines:\n%s" % (pprint.pformat(full_lines), ))
         logger.debug("trailing_excess:\n%s" % (trailing_excess, ))
         
-        
+        # --------------------------------------------------------------------
+        # We now have lots of full lines and some trailing excess. Since a 
+        # log datum may consist of more than one full line we perform a
+        # similar operation to above. We pass all the full lines to a
+        # function which will generate LogDatum object and return
+        # "trailing excess" full lines.
+        # --------------------------------------------------------------------
+        (log_data, full_lines) = get_log_data_and_excess_lines(full_lines)   
+        for log_datum in log_data:
+            logger.debug("publishing:\n%r" % (log_datum, ))
+            publish_socket.send(str(log_datum))            
+        # --------------------------------------------------------------------
