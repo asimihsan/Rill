82bdccb84bc1a6d57f7c829ba3d7a67df94b26f0
Asim Ihsan <angry.blade5619@ihsan.io>
2012-03-01 23:47:05 +0000

    Don't check in temporary pip installation.

--- bin/cross/web/build/bjoern/bjoern/server.h
+++
@@ -1,4 +0,0 @@
-#include "request.h"
-
-bool server_init(const char* hostaddr, const int port);
-void server_run();
--- bin/cross/web/build/bjoern/bjoern/wsgi.h
+++
@@ -1,8 +0,0 @@
-#include <Python.h>
-#include "request.h"
-
-bool wsgi_call_application(Request*);
-PyObject* wsgi_iterable_get_next_chunk(Request*);
-PyObject* wrap_http_chunk_cruft_around(PyObject* chunk);
-
-PyTypeObject StartResponse_Type;
