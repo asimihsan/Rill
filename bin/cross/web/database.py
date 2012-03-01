4ae6059e2a8b59c9996fee346d60493740a36b62
Asim Ihsan <angry.blade5619@ihsan.io>
2012-03-01 23:46:22 +0000

    web: Finish a draft.

---
+++ bin/cross/web/database.py
@@ -0,0 +1,116 @@
+
+import datetime
+import pymongo
+
+import time
+import functools
+
+# -----------------------------------------------------------------------------
+#   Database constants.
+# -----------------------------------------------------------------------------
+hostnames = ["magpie", "mink", "rabbit", "rat"]
+# -----------------------------------------------------------------------------
+
+class Database(object):
+    one_year = datetime.timedelta(days=365)
+    one_week = datetime.timedelta(days=7)
+    one_day = datetime.timedelta(days=1)
+    six_hours = datetime.timedelta(hours=6)
+    one_hour = datetime.timedelta(hours=1)
+    fifteen_minutes = datetime.timedelta(minutes=15)
+    five_minutes = datetime.timedelta(minutes=5)
+    ten_minutes = datetime.timedelta(minutes=10)
+
+    ngmg_shm_messages_collection_filter = "ngmg_shm_messages"
+    ngmg_messages_collection_filter = "ngmg_messages"
+    ngmgm_ep_collection_filter = "ngmg_ep"
+
+    def __init__(self, database_name=None):
+        if not database_name:
+            database_name = "logs"
+        self.connection = pymongo.Connection(hostnames)
+        self.database = self.connection[database_name]
+
+    def get_collection(self, collection_name):
+        return self.database[collection_name]
+
+    def get_collection_names(self, name_filter=None):
+        all_collection_names = self.database.collection_names()
+        if name_filter:
+            collection_names = [elem for elem in all_collection_names if name_filter in elem]
+        else:
+            collection_names = all_collection_names
+        return collection_names
+
+    def get_ngmg_shm_messages_collections(self):
+        return self.get_collection_names(name_filter = self.ngmg_shm_messages_collection_filter)
+
+    def get_ep_warnings_and_errors(self,
+                                   collection,
+                                   datetime_interval = None,
+                                   fields_to_return = ['contents', 'error_level', 'error_id'],
+                                   fields_to_ignore = ['_id']):
+        search_argument = ['error', 'warning']
+        query_part = {'error_level': {'$in': search_argument}}
+        if not datetime_interval:
+            datetime_interval = self.one_day
+        start_datetime = datetime.datetime.utcnow() - datetime_interval
+        query_part["datetime"] = {"$gt": start_datetime}
+        query_part["error_id"] = {"$exists": True}
+        if fields_to_return:
+            filter_part = dict([(field, 1) for field in fields_to_return])
+        if fields_to_ignore:
+            sub_filter = dict([(field, 0) for field in fields_to_ignore])
+            filter_part.update(sub_filter)
+        results_cursor = collection.find(query_part, filter_part)
+        results_cursor_sorted = results_cursor.sort("datetime", -1)
+        return results_cursor
+
+    def get_full_text_search_of_logs(self,
+                                     collection,
+                                     search_argument,
+                                     datetime_interval = None,
+                                     fields_to_return = ["contents"],
+                                     fields_to_ignore = ["_id"]):
+        query_part = {'keywords': {'$all': search_argument}}
+        if not datetime_interval:
+            datetime_interval = self.one_day
+        start_datetime = datetime.datetime.utcnow() - datetime_interval
+        query_part["datetime"] = {"$gt": start_datetime}
+        if fields_to_return:
+            filter_part = dict([(field, 1) for field in fields_to_return])
+        if fields_to_ignore:
+            sub_filter = dict([(field, 0) for field in fields_to_ignore])
+            filter_part.update(sub_filter)
+        results_cursor = collection.find(query_part, filter_part)
+        results_cursor_sorted = results_cursor.sort("datetime", -1)
+        return results_cursor
+
+    def get_shm_split_brain_logs(self,
+                                 collection,
+                                 datetime_interval=None,
+                                 fields_to_return = ["contents"],
+                                 fields_to_ignore = ["_id"]):
+        search_argument = ['brain']
+        return self.get_full_text_search_of_logs(collection,
+                                                 search_argument,
+                                                 datetime_interval,
+                                                 fields_to_return,
+                                                 fields_to_ignore)
+
+    def get_all_items_from_collection_newer_than(self,
+                                                 collection_name,
+                                                 datetime_interval,
+                                                 fields_to_return = ["query_datetime", "tree"],
+                                                 fields_to_ignore = ["_id"]):
+        now = datetime.datetime.utcnow()
+        collection = self.get_read_collection(collection_name)
+        query_part = {"query_datetime": {"$gt": now - datetime_interval}}
+        if fields_to_return:
+            filter_part = dict([(field, 1) for field in fields_to_return])
+        if fields_to_ignore:
+            sub_filter = dict([(field, 0) for field in fields_to_ignore])
+            filter_part.update(sub_filter)
+        results_cursor = collection.find(query_part, filter_part)
+        return results_cursor
+
--- bin/cross/web/server.py
+++ bin/cross/web/server.py
@@ -4,8 +4,20 @@ import os
 import sys
 import bottle
 import jinja2
+import pymongo
+import database
+import pprint
+from whoosh.analysis import StandardAnalyzer
+import operator
 
+# ----------------------------------------------------------------------------
+#   Constants.
+# ----------------------------------------------------------------------------
 ROOT_PATH = os.path.abspath(os.path.join(__file__, os.pardir))
+CSS_PATH = os.path.join(ROOT_PATH, "css")
+JS_PATH = os.path.join(ROOT_PATH, "js")
+IMG_PATH = os.path.join(ROOT_PATH, "img")
+# ----------------------------------------------------------------------------
 
 # ----------------------------------------------------------------------------
 #   Templates.
@@ -13,11 +25,128 @@ ROOT_PATH = os.path.abspath(os.path.join(__file__, os.pardir))
 import jinja2
 # ----------------------------------------------------------------------------
 
+# ----------------------------------------------------------------------------
+#   Database connection and queries.
+# ----------------------------------------------------------------------------
+db = database.Database()
+# ----------------------------------------------------------------------------
+
 @bottle.route('/')
 def index():
     data = {}
     return bottle.jinja2_template("index.html", data=data)
 
+@bottle.route('/shm_split_brain')
+def shm_split_brain():
+    collections = sorted(db.get_ngmg_shm_messages_collections())
+    collection_objects = [db.get_collection(collection) for collection in collections]
+    split_brain_log_cursors = [db.get_shm_split_brain_logs(collection, datetime_interval=db.one_day)
+                               for collection in collection_objects]
+    log_data = []
+    for (collection, split_brain_log_cursor) in zip(collections, split_brain_log_cursors):
+        results = [elem for elem in split_brain_log_cursor]
+        log_data.append((collection, results))
+    return bottle.jinja2_template("shm_split_brain.html",
+                                  log_data=log_data)
+
+@bottle.route('/ep_error_count')
+def ep_error_count():
+    return bottle.jinja2_template("ep_error_count.html")
+
+@bottle.route('/ep_error_count', method='POST')
+def ep_error_count():
+    # ------------------------------------------------------------------------
+    #   Validate inputs.
+    # ------------------------------------------------------------------------
+    valid_intervals = set(["one_hour", "six_hours", "one_day", "one_week"])
+    datetime_interval = bottle.request.forms.get("datetime_interval")
+    assert(datetime_interval in valid_intervals)
+    # ------------------------------------------------------------------------
+
+    # ------------------------------------------------------------------------
+    #   Get raw results.
+    #   !!AI in time use map reduce. For now bang it out.
+    # ------------------------------------------------------------------------
+    datetime_interval_obj = getattr(db, datetime_interval)
+    log_type = "ngmg_ep"
+    collection_names = sorted(db.get_collection_names(name_filter = log_type))
+    log_data = []
+    for collection_name in collection_names:
+        collection = db.get_collection(collection_name)
+        cursor = db.get_ep_warnings_and_errors(collection = collection,
+                                               datetime_interval = datetime_interval_obj)
+        results = [elem for elem in cursor]
+        log_data.append((collection_name, results))
+    # ------------------------------------------------------------------------
+
+    summarized_error_data = {}
+    summarized_warning_data = {}
+    for (collection_name, results) in log_data:
+        collection_results = {}
+        for result in results:
+            error_id = result["error_id"]
+            if result["error_level"] == "warning":
+                summarized_warning_data[error_id] = summarized_warning_data.get(error_id, 0) + 1
+            elif result["error_level"] == "error":
+                summarized_error_data[error_id] = summarized_error_data.get(error_id, 0) + 1
+
+    sorted_warning_data = summarized_warning_data.items()
+    sorted_warning_data.sort(key=operator.itemgetter(1), reverse=True)
+    sorted_error_data = summarized_error_data.items()
+    sorted_error_data.sort(key=operator.itemgetter(1), reverse=True)
+
+    pprint.pprint(sorted_warning_data)
+    pprint.pprint(sorted_error_data)
+    return bottle.jinja2_template("ep_error_count_results.html",
+                                  log_data = log_data,
+                                  sorted_warning_data = sorted_warning_data,
+                                  sorted_error_data = sorted_error_data,
+                                  datetime_interval = datetime_interval)
+
+@bottle.route('/full_text_search')
+def full_text_search():
+    data = {}
+    return bottle.jinja2_template("full_text_search.html", data=data)
+
+@bottle.route('/full_text_search', method='POST')
+def full_text_search_results():
+    # ------------------------------------------------------------------------
+    #   Validate inputs.
+    # ------------------------------------------------------------------------
+    valid_types = set(["ngmg_shm_messages", "ngmg_ep", "ngmg_messages"])
+    valid_intervals = set(["one_hour", "six_hours", "one_day", "one_week"])
+
+    search_string = bottle.request.forms.get("search_string")
+    log_type = bottle.request.forms.get("log_type")
+    datetime_interval = bottle.request.forms.get("datetime_interval")
+
+    assert(log_type in valid_types)
+    assert(datetime_interval in valid_intervals)
+    # ------------------------------------------------------------------------
+
+    # ------------------------------------------------------------------------
+    #   Tokenize the search string.
+    # ------------------------------------------------------------------------
+    analyzer = StandardAnalyzer()
+    tokens = []
+    for elem in analyzer(unicode(search_string)):
+        tokens.append(elem.text)
+    tokens = list(set(tokens))
+    print "search_string: %s, log_type: %s, datetime_interval: %s, tokens: %s" % (search_string, log_type, datetime_interval, tokens)
+    # ------------------------------------------------------------------------
+
+    datetime_interval_obj = getattr(db, datetime_interval)
+    collection_names = sorted(db.get_collection_names(name_filter = log_type))
+    log_data = []
+    for collection_name in collection_names:
+        collection = db.get_collection(collection_name)
+        cursor = db.get_full_text_search_of_logs(collection = collection,
+                                                 search_argument = tokens,
+                                                 datetime_interval = datetime_interval_obj)
+        results = [elem for elem in cursor]
+        log_data.append((collection_name, results))
+    return bottle.jinja2_template("full_text_search_results.html",
+                                  log_data = log_data)
 # ----------------------------------------------------------------------------
 #   Static files.
 # ----------------------------------------------------------------------------
@@ -27,13 +156,22 @@ def server_static():
 
 @bottle.route('/css/<filename>')
 def server_css_static(filename):
-    return bottle.static_file(filename, root=os.path.join(ROOT_PATH, "css"))
+    return bottle.static_file(filename, root=CSS_PATH)
+
+@bottle.route('/img/<filename>')
+def server_css_static(filename):
+    return bottle.static_file(filename, root=IMG_PATH)
 
 @bottle.route('/js/<filepath:path>')
 def server_js_static(filepath):
-    return bottle.static_file(filepath, root=os.path.join(ROOT_PATH, "js"))
+    return bottle.static_file(filepath, root=JS_PATH)
 # ----------------------------------------------------------------------------
 
+bottle.run(server='tornado')
+
 bottle.debug(True)
-bottle.run(host="mink", port=8080, reloader=True, interval=0.5)
+bottle.run(host="0.0.0.0",
+           port=8080,
+           reloader=True,
+           interval=0.5)
 
