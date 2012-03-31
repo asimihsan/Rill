16c62028e172ec41c9dc1f25275525e0418b9e3e
Asim Ihsan <asim.ihsan@gmail.com>
2012-03-31 18:39:11 +0100

    execute_db_maintenance - Cheeky script to take down, repair, and bring back up each member of the replica set once a week in order to recover disk space in a non-service-impacting manner.

---
+++ execute_db_maintenance.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python2.7
+
+import os
+import sys
+import pdb
+
+import fabric
+from fabric.operations import sudo, run, put
+import fabric.network
+from fabric.api import settings
+
+# -----------------------------------------------------------------------------
+#   Constants.
+# -----------------------------------------------------------------------------
+KEY_FILENAME = r"/root/.ssh/id_rsa.pub"
+APP_NAME = "execute_db_maintenance"
+LOG_FILENAME = r"/var/log/execute_db_maintenance.log"
+USERNAME = "root"
+# -----------------------------------------------------------------------------
+
+# -----------------------------------------------------------------------------
+#   Logging.
+# -----------------------------------------------------------------------------
+import logging
+import logging.handlers
+logger = logging.getLogger(APP_NAME)
+logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(message)s")
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+fh = logging.handlers.RotatingFileHandler(LOG_FILENAME, maxBytes=10*1024*1024, backupCount=10)
+fh.setFormatter(formatter)
+fh.setLevel(logging.DEBUG)
+fh.setFormatter(formatter)
+logger.addHandler(fh)
+# -----------------------------------------------------------------------------
+
+commands = [ \
+             "service mongod stop",
+             "sleep 20",
+             "mongod --dbpath /var/lib/mongo --repair",
+             "chown -R mongod:mongod /var/lib/mongo",
+             "service mongod start",
+             "sleep 20"
+             ]
+hosts = ["magpie", "rabbit", "rat"]
+
+def main():
+    logger = logging.getLogger("%s.main" % (APP_NAME, ))
+    for host in hosts:
+        logging.debug("working on host: %s" % (host, ))
+        with settings(host_string = host,
+                      user = USERNAME,
+                      key_filename = KEY_FILENAME):
+            for command in commands:
+                run(command)
+
+if __name__ == "__main__":
+    logging.debug("entry.")
+    main()
+
+
