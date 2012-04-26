adec6206e14f7145bb022bcc35405f2b826a6e3a
Asim Ihsan <asim.ihsan@gmail.com>
2012-04-26 09:34:29 +0100

    execute_db_maintenance - Bug fix. Fabric needs to use RSA private key, not RSA public key.

--- execute_db_maintenance.py
+++ execute_db_maintenance.py
@@ -13,7 +13,7 @@ from fabric.api import settings
 # -----------------------------------------------------------------------------
 #   Constants.
 # -----------------------------------------------------------------------------
-KEY_FILENAME = r"/root/.ssh/id_rsa.pub"
+KEY_FILENAME = r"/root/.ssh/id_rsa"
 APP_NAME = "execute_db_maintenance"
 LOG_FILENAME = r"/var/log/execute_db_maintenance.log"
 USERNAME = "root"
@@ -40,16 +40,15 @@ logger.addHandler(fh)
 # -----------------------------------------------------------------------------
 
 commands = [ \
+             "mongo admin --eval \"printjson(db.adminCommand({replSetStepDown : 60}))\"",
+             "sleep 60",
              "service mongod stop",
-             "sleep 10",
-             #"pkill -9 mongod",
+             "pkill -9 mongod",
              "numactl --interleave=all mongod --dbpath /var/lib/mongo --repair < /dev/null &> /dev/null",
-             "service mongod stop",
              "chown -R mongod:mongod /var/lib/mongo",
              "service mongod start",
-             "sleep 10"
              ]
-hosts = ["magpie", "rabbit", "rat", "fox"]
+hosts = ["magpie", "rat", "fox", "rabbit"]
 
 def main():
     logger = logging.getLogger("%s.main" % (APP_NAME, ))
@@ -57,13 +56,18 @@ def main():
         logger.debug("working on host: %s" % (host, ))
         with settings(host_string = host,
                       user = USERNAME,
-                      key_filename = KEY_FILENAME):
+                      key_filename = KEY_FILENAME,
+                      warn_only = True):
             for command in commands:
+                logger.debug("executing command: %s" % (command, ))
                 run(command)
 
 if __name__ == "__main__":
     logger.debug("entry. starting in 5 seconds...")
     time.sleep(5)
-    main()
-
+    try:
+        main()
+    except:
+        logger.exception("unhandled exception")
+        sys.exit(1)
 
