d17b22001360268c4f810914145f29d06f32326b
Asim Ihsan <asim.ihsan@gmail.com>
2012-07-03 16:14:33 +0100

    age_database - changes to date

--- bin/cross/age_database.py
+++ bin/cross/age_database.py
@@ -15,11 +15,16 @@ import database
 import datetime
 import pdb
 import time
+import multiprocessing
+import random
+import psutil
+import pymongo
 
 # ----------------------------------------------------------------------------
 #   Constants.
 # ----------------------------------------------------------------------------
 one_day = datetime.timedelta(days=1)
+four_days = datetime.timedelta(days=4)
 five_days = datetime.timedelta(days=5)
 one_week = datetime.timedelta(days=7)
 ten_days = datetime.timedelta(days=10)
@@ -27,8 +32,8 @@ two_weeks = datetime.timedelta(days=14)
 one_minute = datetime.timedelta(minutes=1)
 remove_block_size = 150
 maximum_time_per_collection = 10 * 60 # 10 minutes
-LOG_FILENAME = r"/var/log/age_database.log"
-INTERVAL_SIZE = one_week
+LOG_FILENAME = r"/var/log/rill/age_database.log"
+INTERVAL_SIZE = four_days
 # ----------------------------------------------------------------------------
 
 # -----------------------------------------------------------------------------
@@ -66,9 +71,10 @@ def main():
     database_names = ["logs", "pings", "mv_trees"]
     for database_name in database_names:
         logger.debug("database_name: %s" % (database_name, ))
-        top_db = database.Database(database_name = database_name)
+        top_db = database.Database(database_name = database_name, logger = logger)
         write_database = top_db.write_database
-        collection_names = sorted(write_database.collection_names())
+        collection_names = write_database.collection_names()
+        random.shuffle(collection_names)
         if "system.indexes" in collection_names:
             collection_names.remove("system.indexes")
         if database_name == "mv_trees":
@@ -77,29 +83,52 @@ def main():
             datetime_query_string = "datetime"
         for collection_name in collection_names:
             logger.debug("collection_name: %s" % (collection_name, ))
+            logger = logging.getLogger("%s.main.%s" % (APP_NAME, collection_name, ))
             start_time = time.time()
             oldest_date = datetime.datetime.utcnow() - INTERVAL_SIZE
             newest_date = datetime.datetime.utcnow() + one_day
+
+            collection = write_database[collection_name]
+            cursor = collection.find({"$or": [{datetime_query_string: {'$lt': oldest_date}},
+                                              {datetime_query_string: {'$gt': newest_date}}]},
+                                     {'_id': 1}).sort(datetime_query_string, 1) #.limit(remove_block_size)
+            logger.debug("total number of documents: %s. documents to delete remaining: %s" % (collection.count(), cursor.count()))
+            count = cursor.count(with_limit_and_skip = True)
+            collection_cnt = 0
             while (time.time() - start_time) <= maximum_time_per_collection:
-                collection = write_database[collection_name]
-                cursor = collection.find({"$or": [{datetime_query_string: {'$lt': oldest_date}},
-                                                  {datetime_query_string: {'$gt': newest_date}}]},
-                                         {'_id': 1}).sort(datetime_query_string, 1).limit(remove_block_size)
-                logger.debug("collection: %s. total number of documents: %s. documents to delete remaining: %s" % (collection_name, collection.count(), cursor.count()))
-                count = cursor.count(with_limit_and_skip = True)
-                if count == 0:
-                    logger.debug("finished with current collection.")
-                    break
-                ids = [elem['_id'] for elem in cursor]
-                remove_rc = collection.remove({'_id': {'$in': ids}}, safe = True)
+                logger.debug("collection_cnt: %s" % collection_cnt)
+                is_collection_finished = False
+                ids = []
+                try:
+                    cnt = 0
+                    while cnt < remove_block_size:
+                        ids.append(cursor.next()['_id'])
+                        cnt += 1
+                except StopIteration:
+                    logger.debug("no more rows left.")
+                    is_collection_finished = True
+                remove_rc = collection.remove({'_id': {'$in': ids}},
+                                              safe = True,
+                                              w = "majority")
                 logger.debug("remove_rc: %s" % (remove_rc, ))
-                if remove_rc["n"] == 0:
-                    logger.error("Failed to delete any rows! Skip collection for now.")
+                if is_collection_finished:
+                    logger.debug("collection is finished.")
                     break
+                collection_cnt += 1
 
 if __name__ == "__main__":
     try:
-        main()
+        p = multiprocessing.Process(target = main)
+        time_start = time.time()
+        p.daemon = True
+        p.start()
+        psutil.Process(p.pid).nice = 19
+        timeout = 60 * 60 # 60 minutes
+        while p.is_alive() and ((time.time() - time_start) <= timeout):
+            time.sleep(1)
+        if p.is_alive():
+            logger.error("command is still running, taking too long.")
+        p.terminate()
     except:
         logger.exception("Unhandled exception.")
 
